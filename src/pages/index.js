ecadbefb42bf1ea3cec64f11ede3409b53883c61
Rafael Montenegro <rafaelmon14@gmail.com>
2025-03-25 13:05:01 -0500

    add react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { StaticImage, GatsbyImage, getImage} from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 import { graphql, Link } from "gatsby"
+import { Helmet } from "react-helmet"
 
 export default function Home({data}) {
   // const image = getImage(data.file.childImageSharp)
@@ -10,8 +11,10 @@ export default function Home({data}) {
   // console.log("eta vaina e seria: ", image);
   return (
     <Layout>
-      <meta name="google-site-verification" content="yekFUIh4YbV_v1F97NIJ3ZbwgX5oFWfJDMfGLg2ZRMo" />
-      <title>Rafael Montenegro - Portfolio Web</title>
+      <Helmet>
+        <meta name="google-site-verification" content="yekFUIh4YbV_v1F97NIJ3ZbwgX5oFWfJDMfGLg2ZRMo" />
+        <title>Rafael Montenegro - Portfolio Web</title>
+      </Helmet>
       <section className={styles.header}>
         <div>
           <h2>Rafael Montenegro</h2>
